0862374c12cd35cd5a6ef627c4475b8e8526bf1a
Prajnya.Satish <prajnya.12@gmail.com>
2018-03-22 03:17:37 +0530

    Added OptionFinder interface

---
+++ SourceFiles/src/OptionFinder.java
@@ -0,0 +1,13 @@
+/**
+ * Interface that is used to check availability and display the options available to the customer.
+ * It is used for any methds that check the database for info about the options
+ */
+
+/**
+ * @author prajnya
+ *
+ */
+interface OptionFinder {
+	int checkAvailability();
+	void displayOptions();
+}
