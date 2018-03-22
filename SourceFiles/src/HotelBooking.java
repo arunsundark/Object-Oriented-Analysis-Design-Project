865ea7ddfac9465d71c981f705da8fadd772a8a1
Ashish <asta0463@colorado.edu>
2018-03-21 19:42:41 -0600

    Committing the concrete class for Hotel Booking

---
+++ SourceFiles/src/HotelBooking.java
@@ -0,0 +1,36 @@
+/**
+ * @author ashish
+ *
+ */
+
+import java.util.*;
+
+
+public class HotelBooking extends HotelBasicDetails {
+	private String customerId;
+	
+	public String getCustomerId() {
+		return customerId;
+	}
+	
+	public int makeHotelSelection(HotelBooking[] list) {
+		int choice=0;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Please make your selection from the options below: (0 to cancel)");
+		for (int i=0; i<list.length; i++) {
+			System.out.println(Integer.toString(i+1) + "\t" +list[i].hotelName + "\t" + list[i].location + "\t" + Integer.toString(list[i].price));
+		}
+		choice=sc.nextInt();
+		return choice;
+	}
+	
+	public void displayHotelConfirmation() {
+		System.out.println("Your Hotel Booking is confirmed");
+		System.out.println("Customer ID: "+ customerId);
+		System.out.println("Hotel: "+ hotelName);
+		System.out.println("Location: "+ location);
+		System.out.println("Booking date: "+ date.toString());
+		System.out.println("Booked for " + Integer.toString(numDays));
+		System.out.println("Total price: " + Integer.toString(price));
+	}
+}
